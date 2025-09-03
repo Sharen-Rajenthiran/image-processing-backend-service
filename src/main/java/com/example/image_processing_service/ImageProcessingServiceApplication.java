fc711bdb9d227415331014d0e0d0e275d1cbe9c5
Sharen Rajenthiran <sharenrajenthiran@gmail.com>
2025-09-03 18:34:57 +0800

    setup springboot

---
+++ src/main/java/com/example/image_processing_service/ImageProcessingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.image_processing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingServiceApplication.class, args);
+	}
+
+}
